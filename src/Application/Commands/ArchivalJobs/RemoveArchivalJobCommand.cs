54ec37a564de8220283784ac7ccbf263325f4fe5
MaxHotiuk <maksym.hotiuk@lnu.edu.ua>
2025-07-18 15:03:53 +0300

    style: Adjust formatting

--- src/Application/Commands/ArchivalJobs/RemoveArchivalJobCommand.cs
+++ src/Application/Commands/ArchivalJobs/RemoveArchivalJobCommand.cs
@@ -1,6 +1,5 @@
 using MediatR;
 
-namespace Application.Commands.ArchivalJobs
-{
-    public record RemoveArchivalJobCommand(Guid JobId) : IRequest<bool>;
-}
+namespace Application.Commands.ArchivalJobs;
+
+public record RemoveArchivalJobCommand(Guid JobId) : IRequest<bool>;
--- src/Application/Commands/ArchivalJobs/RemoveArchivalJobCommandHandler.cs
+++ src/Application/Commands/ArchivalJobs/RemoveArchivalJobCommandHandler.cs
@@ -1,25 +1,19 @@
-using System.Threading;
-using System.Threading.Tasks;
-using Application.Commands.ArchivalJobs;
 using MediatR;
-using Application.Abstractions.Archivation;
-using Application.Common.Dtos.Boards;
 using Application.Abstractions.Persistence;
 
-namespace Application.Commands.ArchivalJobs
+namespace Application.Commands.ArchivalJobs;
+
+public class RemoveArchivalJobCommandHandler : IRequestHandler<RemoveArchivalJobCommand, bool>
 {
-    public class RemoveArchivalJobCommandHandler : IRequestHandler<RemoveArchivalJobCommand, bool>
+    private readonly ICosmosArchivalJobRepository _archivalJobRepository;
+
+    public RemoveArchivalJobCommandHandler(ICosmosArchivalJobRepository archivalJobRepository)
     {
-        private readonly ICosmosArchivalJobRepository _archivalJobRepository;
+        _archivalJobRepository = archivalJobRepository;
+    }
 
-        public RemoveArchivalJobCommandHandler(ICosmosArchivalJobRepository archivalJobRepository)
-        {
-            _archivalJobRepository = archivalJobRepository;
-        }
-
-        public async Task<bool> Handle(RemoveArchivalJobCommand request, CancellationToken cancellationToken)
-        {
-            return await _archivalJobRepository.RemoveJobAsync(request.JobId, cancellationToken);
-        }
+    public async Task<bool> Handle(RemoveArchivalJobCommand request, CancellationToken cancellationToken)
+    {
+        return await _archivalJobRepository.RemoveJobAsync(request.JobId, cancellationToken);
     }
 }
